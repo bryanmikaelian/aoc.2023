6e9830ba061fe5c8c0cc3b68e72854889b4a6b58
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2023-12-03 16:15:16 -0500

    Cleanup

--- src/aoc/core.clj
+++ src/aoc/core.clj
@@ -2,16 +2,13 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as str])
 
-  (:import Character
-           Integer)
+  (:import java.lang.Character
+           java.lang.Integer)
 
   (:gen-class))
 
-(def d1-input (io/resource "day1.txt"))
-
-
-(def raw-calibrations
-  (with-open [r (io/reader d1-input)]
+(def ^:private raw-calibrations
+  (with-open [r (io/reader (io/resource "day1.txt"))]
     (let [c (line-seq r)
           d (into [] c)]
       d)))
@@ -22,12 +19,18 @@
 (defn- join-digits [s]
   (let [f (first s)
         l (last s)]
-   (Integer/parseInt (str/join [f l]))))
+    (Integer/parseInt (str/join [f l]))))
 
 (defn- calibration-total []
   (let [nums (map strip raw-calibrations)
         calibrations (map join-digits nums)]
     (reduce + calibrations)))
 
-(defn -main [_]
-  (calibration-total))
+(defn run []
+  (prn "aoc 2023 solutions")
+  (prn "------------------")
+  (prn "day 1 part 1 answer" (calibration-total))
+  (prn "day 1 part 2 answer" (calibration-total)))
+
+(comment
+  (run))
