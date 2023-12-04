939e64563403bd628b7e0d96bc516ba6a8b95e0c
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2023-12-03 21:49:10 -0500

    Finish Part 2

--- src/aoc/core.clj
+++ src/aoc/core.clj
@@ -1,11 +1,31 @@
 (ns aoc.core
   (:require [clojure.java.io :as io]
-            [clojure.string :as str])
+            [clojure.string :as string])
 
-  (:import java.lang.Character
-           java.lang.Integer)
+  (:import java.lang.Integer))
 
-  (:gen-class))
+(def ^:private strings [["one" 1]
+                        ["two" 2]
+                        ["three" 3]
+                        ["four" 4]
+                        ["five" 5]
+                        ["six" 6]
+                        ["seven" 7]
+                        ["eight" 8]
+                        ["nine" 9]])
+
+(def ^:private numbers [["1" 1]
+                        ["2" 2]
+                        ["3" 3]
+                        ["4" 4]
+                        ["5" 5]
+                        ["6" 6]
+                        ["7" 7]
+                        ["8" 8]
+                        ["9" 9]])
+
+(def ^:private mapping (into {} (concat strings numbers)))
+(def ^:private num-matcher (re-pattern (str "(?=(\\d|" (string/join "|" (map first strings)) "))")))
 
 (def ^:private raw-calibrations
   (with-open [r (io/reader (io/resource "day1.txt"))]
@@ -13,24 +33,31 @@
           d (into [] c)]
       d)))
 
-(defn- strip [s]
-  (apply str (filter #(Character/isDigit %) s)))
+(defn- numbers-only [s]
+  (re-seq #"\d" s))
+
+(defn- numberlike-values [s]
+  (map last (re-seq num-matcher s)))
 
 (defn- join-digits [s]
   (let [f (first s)
         l (last s)]
-    (Integer/parseInt (str/join [f l]))))
+    (Integer/parseInt (string/join [f l]))))
 
-(defn- calibration-total []
-  (let [nums (map strip raw-calibrations)
-        calibrations (map join-digits nums)]
-    (reduce + calibrations)))
+(defn convert [s]
+  (join-digits (map mapping s)))
 
-(defn run []
+(def ^:private digits-only (map numbers-only raw-calibrations))
+(def ^:private all-num-like-values (map numberlike-values raw-calibrations))
+
+(defn- calibrate [nums]
+  (reduce + (map convert nums)))
+
+(defn run [_]
   (prn "aoc 2023 solutions")
   (prn "------------------")
-  (prn "day 1 part 1 answer" (calibration-total))
-  (prn "day 1 part 2 answer" (calibration-total)))
+  (prn "day 1 part 1 answer" (calibrate digits-only))
+  (prn "day 1 part 2 answer" (calibrate all-num-like-values)))
 
 (comment
-  (run))
+  (run nil))
